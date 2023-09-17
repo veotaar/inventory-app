1e5bd3f5a3a3c08a4dd7686fc0bf30433481d504
Yalcincan <41164799+veotaar@users.noreply.github.com>
2023-09-18 01:15:00 +0300

    Add controller and view for listing single item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -18,4 +18,45 @@ exports.item_list = asyncHandler(async (req, res, next) => {
   const allItems = await Item.find({}, 'name category').sort({ name: 1 }).populate('category').exec();
 
   res.render('item_list', {title: 'Item List', item_list: allItems});
+});
+
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: item create get');
+});
+
+exports.item_create_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: item create post');
+});
+
+exports.item_delete_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: item delete get');
+});
+
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: item delete post');
+});
+
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: item update get');
+});
+
+exports.item_update_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: item update post');
+});
+
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate('category').exec();
+
+  if (item === null) {
+    // No results.
+    const err = new Error('Book not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('item_detail', {
+    title: item.name,
+    item: item
+  })
+
 })
