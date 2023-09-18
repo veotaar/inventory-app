744459b448dd5342bf43a79e16f91c4bd0867781
Yalcincan <41164799+veotaar@users.noreply.github.com>
2023-09-18 19:40:40 +0300

    Add controller and view for listing a category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,5 +8,49 @@ exports.category_list = asyncHandler(async (req, res, next) => {
     .sort({ name: 1 })
     .exec();
 
-  res.render('category_list', { title: 'Item List', category_list: allCategories });
+  res.render('category_list', { title: 'Category List', category_list: allCategories });
+});
+
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: category create get');
+});
+
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: category create post');
+});
+
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: category delete get');
+});
+
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: category delete post');
+});
+
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: category update get');
+});
+
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: category update post');
+});
+
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({category: req.params.id}, 'name description').exec(),
+  ]);
+
+  if (category === null) {
+    // No results.
+    const err = new Error('Category not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('category_detail', {
+    title: 'Category Detail',
+    category: category,
+    category_items: itemsInCategory,
+  });
 });
--- routes/catalog.js
+++ routes/catalog.js
@@ -21,6 +21,19 @@ router.get('/item/:id', item_controller.item_detail);
 
 router.get('/items', item_controller.item_list);
 
+// category routes
+
+router.get('/category/create', category_controller.category_create_get);
+router.post('/category/create', category_controller.category_create_post);
+
+router.get('/category/:id/delete', category_controller.category_delete_get);
+router.post('/category/:id/delete', category_controller.category_delete_post);
+
+router.get('/category/:id/update', category_controller.category_update_get);
+router.post('/category/:id/update', category_controller.category_update_post);
+
+router.get('/category/:id', category_controller.category_detail);
+
 router.get('/categories', category_controller.category_list);
 
 module.exports = router;
